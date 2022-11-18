be03f3708d8650acdd654a4fd9549a04171fcc89
Ochilenovo <rojasvicto745@gmail.comm>
2022-11-18 20:34:28 -0300

    prueba de guardado en index

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <title>Proyecto Web | THN Py S.A</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="">
+    <meta name="author" content="System" />
+    <meta name="author" content="Victor Rojas" />
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="assets/img/favicon.ico" />
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="assets/plugins/font-awesome-4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="assets/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="assets/plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
+
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body class="login-page bg-login" background="assets/img/fondo.jpg">
+  <br>
+    <div class="login-box">
+      <div style="color:#2ECCFA" class="login-logo">
+        <img style="margin-top:-15px" src="assets/img/ms-icon-70x70.png" alt="Logo" > <b>THN Py S.A </b><!---es efav, pone uno de 70x70, chico es la onda-->
+      </div><!-- /.login-logo -->
+      <?php  
+ 
+      if (empty($_GET['alert'])) {
+        echo "";
+      } 
+
+      elseif ($_GET['alert'] == 1) {
+        echo "<div class='alert alert-danger alert-dismissable'>
+                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                <h4>  <i class='icon fa fa-times-circle'></i> Error al entrar!</h4>
+               Usuario o la contraseña es incorrecta, vuelva a verificar su nombre de usuario y contraseña.
+              </div>";
+      }
+
+      elseif ($_GET['alert'] == 2) {
+        echo "<div class='alert alert-success alert-dismissable'>
+                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                <h4>  <i class='icon fa fa-check-circle'></i> Exito!!</h4>
+              Has salido con éxito.
+              </div>";
+      }
+
+      elseif ($_GET['alert'] == 3) {
+        echo "<div class='alert alert-success alert-dismissable'>
+                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                <h4>  <i class='icon fa fa-check-circle'></i> Atención!!</h4>
+             Debes ingresar un usuario y contraseña para interacturar dentro del sistema.
+              </div>";
+      }
+      elseif($_GET['alert']==4){
+        echo "<div class='alert alert-danger alert-dismissable'>
+        <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+        <h4> <i class='icon fa fa-times-circle'></i>Demasiados intentos</h4>
+        El usuario se encuentra bloqueado, por favor, contacte con el Administrador al correo: rojasvictor745@gmail.com
+        </div>";
+    }
+    
+      ?>
+
+      <div class="login-box-body">
+        <p class="login-box-msg"><i class="fa fa-user icon-title"></i> Por favor Inicie Sesión</p>
+        <br/>
+        <form action="login-check.php" method="POST">
+          <div class="form-group has-feedback">
+            <input type="text" class="form-control" name="username" placeholder="Username" autocomplete="off" required />
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+          </div>
+
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" name="password" placeholder="Password" required />
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <br/>
+          <div class="row">
+            <div class="col-xs-12">
+              <input type="submit" class="btn btn-primary btn-lg btn-block btn-flat" name="login" value="Ingresar" />
+            </div><!-- /.col -->
+          </div>
+        </form>
+
+      </div><!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+
+    <!-- jQuery 2.1.3 -->
+    <script src="assets/plugins/jQuery/jQuery-2.1.3.min.js"></script>
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+  </body>
+</html>
